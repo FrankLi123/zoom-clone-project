c52e08f1b236e09adfa64d3223539f8ff081ff4a
FrankLi123 <zoli@ucsd.edu>
2023-06-24 18:53:48 -0400

    Joining room as user and creating new serverless function (check room exists or not)

---
+++ zoom-clone-service/functions/room-exists.js
@@ -0,0 +1,53 @@
+exports.handler = function(context, event, callback) {
+
+    // get client
+
+    const client = context.getTwilioClient();
+
+    const roomId = event.identity; // ??? ??? 
+
+    const response = new Twilio.Response();
+
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": "application/json",
+    };
+  
+    response.setHeaders(headers);
+  
+
+    // check whether a room exists
+    client.video.rooms(roomId).fetch().then( (room) =>{
+
+        if(room) {
+
+            response.setBody({
+                roomExists:true,
+                room
+            });
+        
+        }else{
+        
+            response.setBody({
+                roomExists: false,
+                room
+            })
+
+        }
+
+        return callback(null, response);
+
+    }).catch(err =>{
+
+        response.setBody({
+            roomExists: false,
+            err,
+        });
+    })
+  
+    return callback(null, response);
+  };
+    

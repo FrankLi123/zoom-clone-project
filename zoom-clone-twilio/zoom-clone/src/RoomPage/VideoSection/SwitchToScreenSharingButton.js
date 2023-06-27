a20d995f075af0710d80004d33ec087d223bb2e4
FrankLi123 <zoli@ucsd.edu>
2023-06-27 00:34:06 -0400

    Implemented swtich-to-screen button feature

--- zoom-clone-twilio/zoom-clone/src/RoomPage/VideoSection/SwitchToScreenSharingButton.js
+++ zoom-clone-twilio/zoom-clone/src/RoomPage/VideoSection/SwitchToScreenSharingButton.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+import SwtichButtonImg from '../../resource/switchToScreenSharing.svg';
 
 const SwitchToScreenSharingButton = () => {
 
-    return (
+    const [isScreenSharingActive, setIsScreenSharingActive] = useState(false);
     
-        <div>
+    
+    const handleScreenSHaringEnabling = () => {
+
+        // handle screen sharing
+    }
+    
+    
+    return (
+
+    
+        <div className= "video_button_container" >
+        
+        <img
+        className='video_button_image'
+        src={SwtichButtonImg}
+        onClick={handleScreenSHaringEnabling}
+        >
+        
+        </img>
+        
         </div>
     
         );
